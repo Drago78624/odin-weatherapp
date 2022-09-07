616d2be0d8aea1bd2cea9f73d267b09ca596636d
Drago78624 <maazahmed78624@gmail.com>
2022-09-07 09:32:15 +0500

    ui completed

--- main.js
+++ main.js
@@ -2,3 +2,32 @@ import './style.css'
 
 // endpoint : https://api.weatherapi.com/v1/current.json?key=d4f73fff076641849fa161354220609&q=karachi
 
+const form = document.querySelector(".form")
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const userInput = form.elements['user-input'].value
+    getWeatherData(userInput)
+})
+
+async function getWeatherData(countryName){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=d4f73fff076641849fa161354220609&q=${countryName}`)
+    if(response.status === 200){
+        const data = await response.json()
+        const temperature = data.current.temp_f
+        const condition = data.current.condition.text
+        const conditionIcon = data.current.condition.icon
+        const cloud = data.current.cloud
+        const wind = data.current.wind_mph
+        const pressure = data.current.pressure_mb
+        const humidity = data.current.humidity
+        const feelslike = data.current.feelslike_f
+    }else {
+        console.log("not found");
+    }
+    
+}
+
+
+
